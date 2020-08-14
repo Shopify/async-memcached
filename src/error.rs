51ce86cce367695526f94f446e2dc0ff13b0a5f4
Toby Lawrence <toby@nuclearfurnace.com>
2020-08-14 02:53:17 +0000

    add std::error::Error impl for Error

--- src/error.rs
+++ src/error.rs
@@ -7,6 +7,15 @@ pub enum Error {
     Protocol(Status),
 }
 
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Self::Io(ref e) => Some(e),
+            _ => None,
+        }
+    }
+}
+
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
