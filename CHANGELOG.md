c7fecd42fd19c3ffeb6702f826ff4a8274e5239b
Mark Rattle <mark.rattle@shopify.com>
2024-08-29 23:40:20 -0400

    allow uint value argumenton add method, update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 # Changelog
 
 All notable changes to this project will be documented in this file.
+Keep a running log of your changes with each PR under the `[Unreleased] - ReleaseDate` header.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
@@ -8,6 +9,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 <!-- next-header -->
 
 ## [Unreleased] - ReleaseDate
+
+### Changed
+
+- `set` and `add` methods can now accept `uint`-type argument for value in addition to `&str` and `String` types
 
 ## [0.3.0] - 2024-08-30
 
--- src/lib.rs
+++ src/lib.rs
@@ -258,29 +258,29 @@ impl Client {
     ) -> Result<(), Error>
     where
         K: AsRef<[u8]>,
-        V: AsRef<[u8]>,
+        V: ToMemcachedValue,
     {
         let kr = key.as_ref();
-        let vr = value.as_ref();
 
+        self.conn.write_all(b"add ").await?;
+        self.conn.write_all(kr).await?;
+
+        let flags = flags.unwrap_or(0).to_string();
+        self.conn.write_all(b" ").await?;
+        self.conn.write_all(flags.as_ref()).await?;
+
+        let ttl = ttl.unwrap_or(0).to_string();
+        self.conn.write_all(b" ").await?;
+        self.conn.write_all(ttl.as_ref()).await?;
+
+        self.conn.write_all(b" ").await?;
         self.conn
-            .write_all(
-                &[
-                    b"add ",
-                    kr,
-                    b" ",
-                    flags.unwrap_or(0).to_string().as_ref(),
-                    b" ",
-                    ttl.unwrap_or(0).to_string().as_ref(),
-                    b" ",
-                    vr.len().to_string().as_ref(),
-                    b"\r\n",
-                    vr,
-                    b"\r\n",
-                ]
-                .concat(),
-            )
+            .write_all(value.length().to_string().as_bytes())
             .await?;
+        self.conn.write_all(b"\r\n").await?;
+
+        value.write_to(&mut self.conn).await?;
+        self.conn.write_all(b"\r\n").await?;
         self.conn.flush().await?;
 
         match self.get_read_write_response().await? {
