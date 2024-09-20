1107c1de8e030f58b28e1796b43e4a155737bddc
Nick Amorim <nicholas.amorim@shopify.com>
2024-09-20 10:49:07 -0400

    F

--- examples/tcp.rs
+++ examples/tcp.rs
@@ -6,120 +6,134 @@ async fn main() {
         .await
         .expect("failed to create client");
 
+    let payload = "a".repeat(5 * 1024 * 1024);
+    match client
+        .set_noreply("foo", payload.as_bytes(), None, None)
+        .await
+    {
+        Ok(()) => println!("set 'foo' successfully"),
+        Err(status) => println!("got status during 'foo' set: {:?}", status),
+    }
+
     match client.get("foo").await {
         Ok(value) => println!("got value for 'foo': {:?}", value),
         Err(status) => println!("got status during 'foo' get: {:?}", status),
     }
 
-    match client.set("foo", "might do popeyes", Some(5), None).await {
-        Ok(()) => println!("set 'foo' successfully"),
-        Err(status) => println!("got status during 'foo' set: {:?}", status),
-    }
+    // match client.get("foo").await {
+    //     Ok(value) => println!("got value for 'foo': {:?}", value),
+    //     Err(status) => println!("got status during 'foo' get: {:?}", status),
+    // }
 
-    match client.set("bar", "barvalue", Some(5), None).await {
-        Ok(()) => println!("set 'bar' successfully"),
-        Err(status) => println!("got status during 'bar' set: {:?}", status),
-    }
+    // match client.set("foo", "might do popeyes", Some(5), None).await {
+    //     Ok(()) => println!("set 'foo' successfully"),
+    //     Err(status) => println!("got status during 'foo' set: {:?}", status),
+    // }
 
-    let keys = &["foo", "bar"];
+    // match client.set("bar", "barvalue", Some(5), None).await {
+    //     Ok(()) => println!("set 'bar' successfully"),
+    //     Err(status) => println!("got status during 'bar' set: {:?}", status),
+    // }
 
-    match client.get_multi(keys).await {
-        Ok(values) => println!("got values: {:?}", values),
-        Err(status) => println!("got status during get_many: {:?}", status),
-    }
+    // let keys = &["foo", "bar"];
 
-    match client.add("add_key", "bar", None, None).await {
-        Ok(()) => println!("added 'add_key' successfully"),
-        Err(status) => println!("got status during 'add_key' add: {:?}", status),
-    }
+    // match client.get_multi(keys).await {
+    //     Ok(values) => println!("got values: {:?}", values),
+    //     Err(status) => println!("got status during get_many: {:?}", status),
+    // }
 
-    match client.add("add_key", "bar", None, None).await {
-        Ok(()) => panic!("should not be able to add 'add_key' again"),
-        Err(status) => println!(
-            "duplicate add of 'add_key' fails as expected with: {:?}",
-            status
-        ),
-    }
+    // match client.add("add_key", "bar", None, None).await {
+    //     Ok(()) => println!("added 'add_key' successfully"),
+    //     Err(status) => println!("got status during 'add_key' add: {:?}", status),
+    // }
 
-    match client.delete("foo").await {
-        Ok(()) => println!("deleted 'foo' successfully"),
-        Err(status) => println!("got status during 'foo' delete: {:?}", status),
-    }
+    // match client.add("add_key", "bar", None, None).await {
+    //     Ok(()) => panic!("should not be able to add 'add_key' again"),
+    //     Err(status) => println!(
+    //         "duplicate add of 'add_key' fails as expected with: {:?}",
+    //         status
+    //     ),
+    // }
 
-    match client.delete_no_reply("foo").await {
-        Ok(()) => println!("deleted_no_reply 'foo' successfully"),
-        Err(status) => println!("got status during 'foo' deleted_no_reply: {:?}", status),
-    }
+    // match client.delete("foo").await {
+    //     Ok(()) => println!("deleted 'foo' successfully"),
+    //     Err(status) => println!("got status during 'foo' delete: {:?}", status),
+    // }
 
-    let amount = 1;
-    let increment_key = "increment_key";
+    // match client.delete_no_reply("foo").await {
+    //     Ok(()) => println!("deleted_no_reply 'foo' successfully"),
+    //     Err(status) => println!("got status during 'foo' deleted_no_reply: {:?}", status),
+    // }
 
-    client
-        .set(increment_key, "0", None, None)
-        .await
-        .expect(format!("failed to set {}", increment_key).as_str());
+    // let amount = 1;
+    // let increment_key = "increment_key";
 
-    match client.increment(increment_key, amount).await {
-        Ok(value) => println!(
-            "incremented {} by {} successfully, to {}",
-            increment_key, amount, value
-        ),
-        Err(status) => println!(
-            "got status during increment on key {}: {:?}",
-            increment_key, status
-        ),
-    }
+    // client
+    //     .set(increment_key, "0", None, None)
+    //     .await
+    //     .expect(format!("failed to set {}", increment_key).as_str());
 
-    match client.increment_no_reply(increment_key, amount).await {
-        Ok(()) => println!(
-            "incremented_no_reply {} by {} successfully",
-            increment_key, amount
-        ),
-        Err(status) => println!(
-            "got status during increment_no_reply on {}: {:?}",
-            increment_key, status
-        ),
-    }
+    // match client.increment(increment_key, amount).await {
+    //     Ok(value) => println!(
+    //         "incremented {} by {} successfully, to {}",
+    //         increment_key, amount, value
+    //     ),
+    //     Err(status) => println!(
+    //         "got status during increment on key {}: {:?}",
+    //         increment_key, status
+    //     ),
+    // }
 
-    let decrement_key = "decrement_key";
+    // match client.increment_no_reply(increment_key, amount).await {
+    //     Ok(()) => println!(
+    //         "incremented_no_reply {} by {} successfully",
+    //         increment_key, amount
+    //     ),
+    //     Err(status) => println!(
+    //         "got status during increment_no_reply on {}: {:?}",
+    //         increment_key, status
+    //     ),
+    // }
 
-    client
-        .set(decrement_key, "10", None, None)
-        .await
-        .expect(format!("failed to set {}", decrement_key).as_str());
+    // let decrement_key = "decrement_key";
 
-    match client.decrement(decrement_key, amount).await {
-        Ok(value) => println!(
-            "decremented {} by {} successfully, to {}",
-            decrement_key, amount, value
-        ),
-        Err(status) => println!(
-            "got status during decrement on {}: {:?}",
-            decrement_key, status
-        ),
-    }
+    // client
+    //     .set(decrement_key, "10", None, None)
+    //     .await
+    //     .expect(format!("failed to set {}", decrement_key).as_str());
 
-    match client.decrement_no_reply(decrement_key, amount).await {
-        Ok(()) => println!(
-            "decremented_no_reply {} by {} successfully",
-            decrement_key, amount
-        ),
-        Err(status) => println!(
-            "got status during decrement_no_reply on {}: {:?}",
-            decrement_key, status
-        ),
-    }
+    // match client.decrement(decrement_key, amount).await {
+    //     Ok(value) => println!(
+    //         "decremented {} by {} successfully, to {}",
+    //         decrement_key, amount, value
+    //     ),
+    //     Err(status) => println!(
+    //         "got status during decrement on {}: {:?}",
+    //         decrement_key, status
+    //     ),
+    // }
 
-    match client.version().await {
-        Ok(version) => println!("version {}", version),
-        Err(status) => println!("got status during 'foo' delete: {:?}", status),
-    }
+    // match client.decrement_no_reply(decrement_key, amount).await {
+    //     Ok(()) => println!(
+    //         "decremented_no_reply {} by {} successfully",
+    //         decrement_key, amount
+    //     ),
+    //     Err(status) => println!(
+    //         "got status during decrement_no_reply on {}: {:?}",
+    //         decrement_key, status
+    //     ),
+    // }
 
-    match client.stats().await {
-        Ok(stats) => {
-            println!("got {} stat entries!", stats.len());
-            println!("curr items: {:?}", stats.get("curr_items"));
-        }
-        Err(e) => println!("error while getting stats: {:?}", e),
-    }
+    // match client.version().await {
+    //     Ok(version) => println!("version {}", version),
+    //     Err(status) => println!("got status during 'foo' delete: {:?}", status),
+    // }
+
+    // match client.stats().await {
+    //     Ok(stats) => {
+    //         println!("got {} stat entries!", stats.len());
+    //         println!("curr items: {:?}", stats.get("curr_items"));
+    //     }
+    //     Err(e) => println!("error while getting stats: {:?}", e),
+    // }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -202,6 +202,48 @@ impl Client {
     ///
     /// If `ttl` or `flags` are not specified, they will default to 0.  If the value is set
     /// successfully, `()` is returned, otherwise [`Error`] is returned.
+    pub async fn set_noreply<K, V>(
+        &mut self,
+        key: K,
+        value: V,
+        ttl: Option<i64>,
+        flags: Option<u32>,
+    ) -> Result<(), Error>
+    where
+        K: AsRef<[u8]>,
+        V: AsMemcachedValue,
+    {
+        let kr = key.as_ref();
+        let vr = value.as_bytes();
+
+        self.conn.write_all(b"set ").await?;
+        self.conn.write_all(kr).await?;
+
+        let flags = flags.unwrap_or(0).to_string();
+        self.conn.write_all(b" ").await?;
+        self.conn.write_all(flags.as_ref()).await?;
+
+        let ttl = ttl.unwrap_or(0).to_string();
+        self.conn.write_all(b" ").await?;
+        self.conn.write_all(ttl.as_ref()).await?;
+
+        let vlen = vr.len().to_string();
+        self.conn.write_all(b" ").await?;
+        self.conn.write_all(vlen.as_ref()).await?;
+        self.conn.write_all(b"\r\n").await?;
+
+        self.conn.write_all(vr.as_ref()).await?;
+        self.conn.write_all(b"noreply\r\n").await?;
+
+        self.conn.flush().await?;
+
+        Ok(())
+    }
+
+    /// Sets the given key.
+    ///
+    /// If `ttl` or `flags` are not specified, they will default to 0.  If the value is set
+    /// successfully, `()` is returned, otherwise [`Error`] is returned.
     pub async fn set<K, V>(
         &mut self,
         key: K,
