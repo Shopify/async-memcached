e2485362b0edce3a417e88dad04d4782b9ac84ff
ArtemIsmagilov <artem.ismagilov.2000@mail.ru>
2024-09-24 02:38:40 +0400

    rewrite multi-set command with once generate msg and await

--- src/lib.rs
+++ src/lib.rs
@@ -258,29 +258,28 @@ impl Client {
         K: AsRef<[u8]> + Eq + std::hash::Hash + std::fmt::Debug,
         V: AsMemcachedValue,
     {
+        let mut bf = Vec::new();
+
         for (key, value) in kv {
-            let kr = key.as_ref();
             let vr = value.as_bytes();
-
-            self.conn.write_all(b"set ").await?;
-            self.conn.write_all(kr).await?;
-
-            let flags = flags.unwrap_or(0).to_string();
-            self.conn.write_all(b" ").await?;
-            self.conn.write_all(flags.as_ref()).await?;
-
-            let ttl = ttl.unwrap_or(0).to_string();
-            self.conn.write_all(b" ").await?;
-            self.conn.write_all(ttl.as_ref()).await?;
-
-            let vlen = vr.len().to_string();
-            self.conn.write_all(b" ").await?;
-            self.conn.write_all(vlen.as_ref()).await?;
-            self.conn.write_all(b"\r\n").await?;
-
-            self.conn.write_all(vr.as_ref()).await?;
-            self.conn.write_all(b"\r\n").await?;
+            bf.extend(
+                [
+                    b"set ",
+                    key.as_ref(),
+                    b" ",
+                    flags.unwrap_or(0).to_string().as_ref(),
+                    b" ",
+                    ttl.unwrap_or(0).to_string().as_ref(),
+                    b" ",
+                    vr.len().to_string().as_ref(),
+                    b"\r\n",
+                    vr.as_ref(),
+                    b"\r\n",
+                ]
+                .concat(),
+            );
         }
+        self.conn.write_all(&bf).await?;
         self.conn.flush().await?;
 
         let results = self.map_set_multi_responses(kv).await?;
