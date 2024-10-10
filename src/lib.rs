2b3a78b6e7e19ae1a889e6f2b3b2c7e6b73f3dac
Mark Rattle <mark.rattle@shopify.com>
2024-10-10 12:27:55 -0400

    add initial meta_set implementation

--- src/lib.rs
+++ src/lib.rs
@@ -309,6 +309,69 @@ impl Client {
         }
     }
 
+    /// Sets the given key.
+    ///
+    /// If `ttl` they will default to 0.  If the value is set
+    /// successfully, `Some(Value)` is returned, otherwise [`Error`] is returned.
+    /// NOTE: That the data is some Value is sparsely populated, containing only requested data by meta_flags
+    /// The meta set command a generic command for storing data to memcached. Based
+    /// on the flags supplied, it can replace all storage commands (see token M) as
+    /// well as adds new options.
+    //
+    // ms <key> <datalen> <flags>*\r\n
+    //
+    // - <key> means one key string.
+    // - <datalen> is the length of the payload data.
+    //
+    // - <flags> are a set of single character codes ended with a space or newline.
+    //   flags may have strings after the initial character.
+    pub async fn meta_set<K, V>(
+        &mut self,
+        key: K,
+        value: V,
+        meta_flags: Option<&[&str]>,
+    ) -> Result<Option<Value>, Error>
+    where
+        K: AsRef<[u8]>,
+        V: AsMemcachedValue,
+    {
+        let kr = key.as_ref();
+        let vr = value.as_bytes();
+
+        self.conn.write_all(b"ms ").await?;
+        self.conn.write_all(kr).await?;
+
+        let vlen = vr.len().to_string();
+        self.conn.write_all(b" ").await?;
+        self.conn.write_all(vlen.as_ref()).await?;
+
+        if let Some(meta_flags) = meta_flags {
+            self.conn.write_all(b" ").await?;
+            self.conn.write_all(meta_flags.join(" ").as_bytes()).await?;
+        }
+
+        self.conn.write_all(b"\r\n").await?;
+        self.conn.write_all(vr.as_ref()).await?;
+        self.conn.write_all(b"\r\n").await?;
+        self.conn.flush().await?;
+
+        match self.drive_receive(parse_meta_response).await? {
+            Response::Status(s) => Err(s.into()),
+            Response::Data(d) => d
+                .map(|mut items| {
+                    if items.len() != 1 {
+                        Err(Status::Error(ErrorKind::Protocol(None)).into())
+                    } else {
+                        let mut item = items.remove(0);
+                        item.key = key.as_ref().to_vec();
+                        Ok(item)
+                    }
+                })
+                .transpose(),
+            _ => Err(Error::Protocol(Status::Error(ErrorKind::Protocol(None)))),
+        }
+    }
+
     /// Sets multiple keys and values through pipelined commands.
     ///
     /// If `ttl` or `flags` are not specified, they will default to 0. The same values for `ttl` and `flags` will be applied to each key.
