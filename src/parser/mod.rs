95259f23b99fc84fbba611a01d4f0963a3ce55ce
Mark Rattle <mark.rattle@shopify.com>
2024-10-12 15:03:52 -0400

    initial file split and rename

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -9,12 +9,12 @@ use nom::{
 };
 use std::fmt;
 
-mod ascii;
-pub use ascii::{parse_ascii_metadump_response, parse_ascii_response, parse_ascii_stats_response};
+mod ascii_parser;
+pub use ascii_parser::{parse_ascii_metadump_response, parse_ascii_response, parse_ascii_stats_response};
 
-mod meta;
+mod meta_parser;
 #[allow(unused_imports)]
-pub use meta::{parse_meta_get_response, parse_meta_set_response};
+pub use meta_parser::{parse_meta_get_response, parse_meta_set_response};
 
 /// A value from memcached.
 #[derive(Clone, Debug, PartialEq)]
